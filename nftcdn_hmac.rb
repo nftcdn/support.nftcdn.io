44c46b437c01768445cf0d349ac1fdcb5a3ade41
Smaug <smaug@nftcdn.io>
2025-11-26 18:06:29 +0100

    fix corrupted ruby example

--- nftcdn_hmac.rb
+++ nftcdn_hmac.rb
@@ -1,23 +1,25 @@
 #!/usr/bin/env ruby
 
-require 'openssl'
-require 'Base64'
+require "openssl"
+require "base64"
+require "uri"
 
-def nftcdn_url(domain, key, token, path, params={}):
-    url = build_url(domain, token, path, dict(params, tk=""))
-    mac = base64.urlsafe_b64encode(hmac.new(key, url.encode('ascii'), digestmod=hashlib.sha256).digest())
-    return build_url(domain, token, path, dict(params, tk=mac.decode('ascii').rstrip("=")))
+def nftcdn_url(domain, key, token, path, params = {})
+  url = build_url(domain, token, path, params.merge({ tk: ""}))
+  params[:tk] = Base64.urlsafe_encode64(OpenSSL::HMAC.digest('sha256', key, url), padding: false)
+  return build_url(domain, token, path, params)
 end
 
-def build_url(domain, token, path, params):
-    query = urllib.parse.urlencode(params)
-    return f"https://{token}.{domain}.nftcdn.io{path}?{query}"
+def build_url(domain, token, path, params)
+  query = URI.encode_www_form(params)
+  url = URI::HTTPS.build(host: "#{token}.#{domain}.nftcdn.io", path: path, query: query)
+  return url.to_s
 end
 
 # EXAMPLES
 
 # Your nftcdn.io subdomain and secret key
-(domain, key) = ("preprod", base64.b64decode("7FoxfBgV2k+RSz6UUts3/fG1edG7oIGXxdtIVCdalaI="))
+domain, key = "preprod", Base64.decode64("7FoxfBgV2k+RSz6UUts3/fG1edG7oIGXxdtIVCdalaI=")
 
 token = "asset1cpfcfxay6s73xez8srvhf0pydtd9yqs8hyfawv"
 
@@ -25,7 +27,7 @@ token = "asset1cpfcfxay6s73xez8srvhf0pydtd9yqs8hyfawv"
 puts nftcdn_url(domain, key, token, "/image")
 
 # Resized 256x256 WebP image
-puts nftcdn_url(domain, key, token, "/image", params={'size': 256})
+puts nftcdn_url(domain, key, token, "/image", { size: 256 })
 
 # Metadata
 puts nftcdn_url(domain, key, token, "/metadata")
